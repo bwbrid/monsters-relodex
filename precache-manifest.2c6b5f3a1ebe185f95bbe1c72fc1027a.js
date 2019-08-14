0adf02875a7205e6a35535796d2a8f8372a6ccf4
bwbrid <bwbrid@gmail.com>
2019-08-14 23:25:43 +0900

    Updates

---
+++ precache-manifest.2c6b5f3a1ebe185f95bbe1c72fc1027a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7aba8ee95000c7df8d33f541eb8646",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "ebfd3efffaec229de4bf",
+    "url": "/monsters-relodex/static/css/main.8a45934a.chunk.css"
+  },
+  {
+    "revision": "3828e83819259610d8ed",
+    "url": "/monsters-relodex/static/js/2.7bd8eb7e.chunk.js"
+  },
+  {
+    "revision": "ebfd3efffaec229de4bf",
+    "url": "/monsters-relodex/static/js/main.19291f6e.chunk.js"
+  },
+  {
+    "revision": "9999a5b278e14af20957",
+    "url": "/monsters-relodex/static/js/runtime~main.21fa3eee.js"
+  }
+]);
